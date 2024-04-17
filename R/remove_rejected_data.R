bee2492b0546da0d45eba71712f79775a08d935e
Shahim Nadery <Shahim_Nadery@yahoo.com>
2024-04-18 02:01:43 +0430

    removed SCTO rejected data!

--- R/remove_rejected_data.R
+++ R/remove_rejected_data.R
@@ -1,10 +1,12 @@
 ### Remove Rejected QA status and keys -------------------------------------------------------------
 rejected_qa_status <- "Rejected"
+scto_rejected <- "REJECTED"
 
 ## Tool 1.1 ----------------------------------------------------------------------------------------
-hf_t1_data <- hf_t1_data %>% 
-  filter(KEY %notin% rejection_log$KEY_Unique) %>% 
-  filter(qa_status %notin% rejected_qa_status)
+hf_t1_data <- hf_t1_data %>% count(review_status)
+  filter(qa_status %notin% rejected_qa_status &
+           KEY %notin% rejection_log$KEY_Unique & 
+           review_status %notin% scto_rejected)
 hf_injuries <- hf_injuries %>%
   filter(PARENT_KEY %in% hf_t1_data$KEY) %>%
   filter(KEY %notin% rejection_log$KEY_Unique)
@@ -16,18 +18,24 @@ hf_incidents <- hf_incidents %>%
   filter(KEY %notin% rejection_log$KEY_Unique)
 
 ## Tool 1.2 ----------------------------------------------------------------------------------------
-hf_t2_data <- hf_t2_data %>% filter(qa_status %notin% rejected_qa_status)
+hf_t2_data <- hf_t2_data %>% filter(qa_status %notin% rejected_qa_status & 
+                                      KEY %notin% rejection_log$KEY_Unique & 
+                                      review_status %notin% scto_rejected)
 hf_t2_photos <- hf_t2_photos %>%
   filter(PARENT_KEY %in% hf_t2_data$KEY) %>%
   filter(KEY %notin% rejection_log$KEY_Unique)
 
 ## Tool 1.3 ----------------------------------------------------------------------------------------
 hf_t3_data <- hf_t3_data %>% 
-  filter(qa_status %notin% rejected_qa_status & KEY %notin% rejection_log$KEY_Unique)
+  filter(qa_status %notin% rejected_qa_status & 
+           KEY %notin% rejection_log$KEY_Unique & 
+           review_status %notin% scto_rejected)
  
 ## Tool 2 ------------------------------------------------------------------------------------------
 t2_data <- t2_data %>% 
-  filter(qa_status %notin% rejected_qa_status & KEY %notin% rejection_log$KEY_Unique)
+  filter(qa_status %notin% rejected_qa_status & 
+           KEY %notin% rejection_log$KEY_Unique & 
+           review_status %notin% scto_rejected)
 t2_income <- t2_income %>%
   filter(PARENT_KEY %in% t2_data$KEY) %>% 
   filter(KEY %notin% rejection_log$KEY_Unique)
@@ -46,7 +54,9 @@ t2_other <- t2_other %>%
 
 ## Tool 3 ------------------------------------------------------------------------------------------
 t3_data <- t3_data %>% 
-  filter(qa_status %notin% rejected_qa_status & KEY %notin% rejection_log$KEY_Unique)
+  filter(qa_status %notin% rejected_qa_status & 
+           KEY %notin% rejection_log$KEY_Unique & 
+           review_status %notin% scto_rejected)
 
 ## Remove extra objects ----------------------------------------------------------------------------
 rm(rejected_qa_status)
